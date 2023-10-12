20fa5e1c92c13ae7fa0d31c6342fe42143ad4b2b
Akinwande Aaron <aaronakinwande@gmail.com>
2023-10-12 09:58:58 +0100

    Created Select component and added to home page

---
+++ components/SelectComp.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function SelectComp() {
+  return (
+    <div>
+        <select className=' h-10 w-24 text-purple-500 bg-gray-200 text-sm font-semibold rounded  '>
+            <option value="">Class 1</option>
+            <option value="">Class 2</option>
+            <option value="">Class 3</option>
+        </select> 
+    </div>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,39 @@
-import AuthPages from '@/components/Auth/AuthPages'
-import Image from 'next/image'
-
+import AuthPages from "@/components/Auth/AuthPages";
+import Input from "@/components/FormFields/Input";
+import SelectComp from "@/components/SelectComp";
+import Image from "next/image";
 
 export default function Home() {
+  const handleClick = null;
   return (
-   <AuthPages>
-   Home
-   </AuthPages>
-  )
+    <AuthPages>
+      <div>
+        <div className=" flex flex-col justify-start gap-2  border-b-2 py-4 ">
+          <div className=" text-xl font-bold ">
+            Welcome Back to Musa-School{" "}
+          </div>
+          <div className=" text-lg font-normal">
+            Here are some insights into your daily activities
+          </div>
+        </div>
+        <div className=" flex justify-between mt-5">
+          <div>
+            {" "}
+            <Input
+              label=""
+              type="name"
+              value={""}
+              change={handleClick}
+              id="1"
+              placeholder="Input Name"
+              description=""
+              disabled={false}
+              required
+            ></Input>
+          </div>
+          <div> <SelectComp/></div>
+        </div>
+      </div>
+    </AuthPages>
+  );
 }
