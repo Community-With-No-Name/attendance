7f645113def8fca33248dd9efea8cfab5aa1738a
Jubril Musa <jewbreel1@gmail.com>
2023-11-06 13:44:59 +0100

    donSelector error fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,11 +11,13 @@ const montserrat = Montserrat({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
     
-    return  <div className={montserrat.className+" max-h-screen md:px-10 px-2 md:overflow-hidden pb-4"}>
+    return  (
     <QueryClientProvider client={queryClient}>
-         {/* <Hydrate state={pageProps.dehydratedState}> */}
+         <Hydrate state={pageProps.dehydratedState}>
+    <div className={montserrat.className+" max-h-screen md:px-10 px-2 md:overflow-hidden pb-4"}>
             <Component {...pageProps} />
-         {/* </Hydrate> */}
-        </QueryClientProvider>
             </div>
+         </Hydrate>
+        </QueryClientProvider>
+            )
 }
